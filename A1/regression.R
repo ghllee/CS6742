95b78aa1a022fa266d8d665ff3bf205673fa0237
Jack Hessel <jmhessel@gmail.com>
2014-09-04 22:53:42 -0400

    also updated this file

--- A1/regression.R
+++ A1/regression.R
@@ -1,11 +1,30 @@
+install.packages("plotrix")
+library(plotrix)
 setwd("./Desktop/NLP/git/A1")
-result <- rep(0, 15)
-for(i in 0:14)
+
+n <- 300
+values <- rep(0, n)
+U <- rep(0, n)
+L <- rep(0, n)
+for(i in 0:(n-1))
 {
-  docName <- paste(toString(i), ".csv", sep="")
+  docName <- paste("temp/", toString(i), ".csv", sep="")
   data <- read.csv(docName)
   colnames(data) <- c("Uniqueness", "Helpful")
   myModel <- glm(Helpful ~ Uniqueness, data = data,
                  family = "binomial")
-  result[i] <- coef(summary(myModel))[,"Pr(>|z|)"][2]
+  confInt <- confint(myModel)[2,]
+  L[i] <- confInt[[1]]
+  U[i] <- confInt[[2]]
+  values[i] <- coef(myModel)[[2]]
 }
+productIds <- read.csv("productIds.txt", header=F)
+plotCI(values, ui=U, li=L, pch=".", xlab = "Review Index", ylab = "Influence of Uniqueness",
+        main = "Influence of Uniqueness on Helpfulness for Top 300 Reviews",
+        xlim = c(1,300),
+        ylim = c(-30, 20))
+meanConfInt <- t.test(values)$conf.int
+abline(h=meanConfInt[[1]], col=2, lty=2, lwd = 4)
+abline(h=mean(values), col=2)
+abline(h=meanConfInt[[2]], col=2, lty=2, lwd = 4)
+abline(h=0, col=3, lty=1, lwd = 3)
